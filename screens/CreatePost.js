28ad87a0afc7b61555a1a8f7ff4e85539749eb30
SamyakVars <75443669+SamyakVars@users.noreply.github.com>
2023-01-02 17:11:11 +0100

    Add files via upload

---
+++ screens/CreatePost.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Platform,
+    StatusBar,
+    Image,
+    Dimensions,
+  } from 'react-native';
+
+
+
+export default class App extends React.Component() {
+    render () {
+        return (
+            <View style={styles.container}>
+                <Text> Create Post !</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
