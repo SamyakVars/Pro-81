c998c5e8c0fb07cb61a7eefbd2d615f51b3a14be
SamyakVars <75443669+SamyakVars@users.noreply.github.com>
2023-01-02 17:27:29 +0100

    Update DrawerNavigator.js

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -7,7 +7,7 @@ const Drawer = createDrawerNavigator()
 const DrawerNavigator = () => {
     return (
         <Drawer.Navigator>
-            <Drawer.Screen name="Home" component={TabNavigator}/>
+            <Drawer.Screen name="Home" component={BottomTabNavigator}/>
             <Drawer.Screen name="Profile" component={Profile}/>
 
         </Drawer.Navigator>
